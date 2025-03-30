267b976ba7c86b94968e160a5aaf4817c1bce3f6
alex999alex <150537164+alex999alex@users.noreply.github.com>
2025-03-30 15:54:59 -0300

    Add project files.

---
+++ TheTicketHub/Controllers/TheTicketHubController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace TheTicketHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TheTicketHubController : ControllerBase
+    {
+        private readonly ILogger<TheTicketHubController> _logger;
+        private readonly IConfiguration _configuration;
+        public TheTicketHubController(ILogger<TheTicketHubController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok("Hello from Contacts API - Get()");
+        }
+
+        [HttpPost]
+        //public async Task<IActionResult> Post(Ticket_Hub contact)
+        public IActionResult Post(TheTicketHub theTicketHub)
+        {
+            
+            //1.Validation
+            if (string.IsNullOrEmpty(theTicketHub.Name) || string.IsNullOrEmpty(theTicketHub.Phone))
+            {
+                return BadRequest("Name and Phone are required");
+            }
+            
+            return Ok("Hello" + theTicketHub.Name + " from Post()");
+
+
+
+            //if (ModelState.IsValid == false)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            //// 2. Send contact to Azure queue
+
+            //string queueName = "ticket-hub";
+
+            //// Get connection string from secrets.json
+            //string? connectionString = _configuration["AzureStorageConnectionString"];
+
+            //if (string.IsNullOrEmpty(connectionString))
+            //{
+            //    return BadRequest("An error was encountered");
+            //}
+
+            //QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            //// serialize an object to json
+            //string message = JsonSerializer.Serialize(contact);
+            ////string message = "Hello from API.";
+
+            //// send string message to queue
+            //await queueClient.SendMessageAsync(message);
+
+            //return Ok("Hello " + contact.Name + ". Contact added to Azure queue.");
+        }
+    }
+}
---
+++ TheTicketHub/TheTicketHub.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheTicketHub
+{
+    public class TheTicketHub
+    {
+
+        //[Required(ErrorMessage = "First Name is required.")]
+        //public string FirstName { get; set; } = string.Empty;
+
+        //[MaxLength(10, ErrorMessage = "Last Name need to be maxiaum length of 10 charecters.")]
+        //[Required(ErrorMessage = "Last Name is required.")]
+        //public string LastName { get; set; } = string.Empty;
+
+        //[EmailAddress]
+        //[Required]
+        //public string Email { get; set; } = string.Empty;
+
+
+
+        //[Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        //[MaxLength(10, ErrorMessage = "Phone need to be maxiaum length of 10 charecters.")]
+        //[Required(ErrorMessage = "Phone is required.")]
+        public string Phone { get; set; } = string.Empty;
+
+        //[EmailAddress]
+        //[Required]
+        //public string Email { get; set; } = string.Empty;
+    }
+}
