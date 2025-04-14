da28d9ec1a0fba23ac517b9831bc10036ce2f31f
alex999alex <150537164+alex999alex@users.noreply.github.com>
2025-04-14 17:59:03 -0300

    11

--- TheTicketHub/Program.cs
+++ TheTicketHub/Program.cs
@@ -7,6 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowStaticWebApp", policy =>
+    {
+        policy.WithOrigins("https://blue-cliff-01d2a550f.6.azurestaticapps.net")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Add user secrets
